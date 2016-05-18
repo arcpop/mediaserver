1186b703f172fcd189c11bb6673dc011f4f34ada
Raffaele Meyer <rafmeyer91@gmail.com>
2016-05-18 12:29:46 +0200

    Update

--- user_handler.go
+++ user_handler.go
@@ -2,10 +2,11 @@ package mediaserver
 
 
 import (
+    "fmt"
     "net/http"
 )
 
 
 func (srv* Server) userHandler(w http.ResponseWriter, r *http.Request, s *session) {
-    
+    fmt.Fprintln(w, "Hello world!")
 }
